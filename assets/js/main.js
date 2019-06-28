ca4c2abe709f06f9f866942a61f4cf569857c993
unknown <Digital@DH-A2-34.dhbrasil.digitalhouse.com>
2019-06-27 22:05:36 -0300

    lista-tarefas atualizadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,16 @@ const listarTarefas = (elementoHtml) => {
     fetch('http://localhost:8000/api/listar-tarefas')
     .then(resposta => resposta.json())
     .then(dados => {
+        dados = dados.reverse()
         let html = ''
 
         dados.forEach(dado => {
+            let span = dado.status == 'feito'
+            ? `<span style="text-decoration: line-through">${dado.conteudo}</span>`
+            : `<span>${dado.conteudo}</span>`
             html+= `
                 <li class="list-group-item d-flex">
-                    <span id="conteudoTarefa">${dado.conteudo}</span>
+                    ${span}
                     <div class="ml-auto">
                         <a id="ConcluirTarefa" tarefa-id="${dado.id}" href="#">
                             <i title="Concluir tarefa" class="material-icons">done</i>
@@ -24,6 +28,10 @@ const listarTarefas = (elementoHtml) => {
             `
         })
         elementoHtml.innerHTML = html
+    }).then(() => {
+        adicionarEventoConcluir()
+        adicionarEventoDeletar()
+
     })
 }
 
@@ -35,6 +43,53 @@ const adicionaTarefa = (tarefa) => {
 		method:'post',
 		body: dados
 	}).then(resposta => resposta.json()).then(dados => console.log(dados))
+}
+
+const adicionarEventoConcluir = () => {
+    let botaoConcluir = document.querySelectorAll('#ConcluirTarefa')
+
+    botaoConcluir.forEach(botoesConcluir => {
+        botoesConcluir.onclick = (evento) => {
+            evento.preventDefault()
+
+            fetch(`http://localhost:8000/api/atualizar-tarefa/${botoesConcluir.getAttribute('tarefa-id')}`, {
+                method: 'put'
+            })
+            .then(resposta => resposta.json())
+            .then(dados => {
+                if(dados.status == 'feito') {
+                    botoesConcluir.parentElement.previousElementSibling.style.textDecoration = 'line-through'
+                } else {
+                    botoesConcluir.parentElement.previousElementSibling.style.textDecoration = 'none'
+                }
+            })
+        }
+    })
+}
+
+const adicionarEventoDeletar = () => {
+    let botaoDeletar = document.querySelectorAll('#deletarTarefa')
+
+    botaoDeletar.forEach(botoesDeletar => {
+        botoesDeletar.onclick = (evento) => {
+            evento.preventDefault()
+
+            let respostaUsuario = confirm('Quer deletar essa tarefa?')
+
+            if(respostaUsuario)
+
+            fetch(`http://localhost:8000/api/deletar-tarefa/${botoesDeletar.getAttribute('tarefa-id')}`, {
+                method: 'delete'
+            })
+            .then(resposta => resposta.json())
+            .then(dados => {
+                if(dados) {
+                    botoesDeletar.parentElement.parentElement.classList.remove('d-flex')
+                    botoesDeletar.parentElement.parentElement.classList.add('d-none')
+                } 
+            })
+        }
+    })
 }
 
 fomulario.onsubmit = (evento) => {
